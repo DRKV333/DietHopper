2e4fc1bb168a4bc61b46d1a3592c7c6b878f6121
Kálmán Vince <30904009+DRKV333@users.noreply.github.com>
2025-04-02 23:11:49 +0200

    Fixed returned collision wrong

--- src/main/java/com/rwtema/diethopper/BlockDietHopper.java
+++ src/main/java/com/rwtema/diethopper/BlockDietHopper.java
@@ -10,37 +10,54 @@ import net.minecraft.world.World;
 public class BlockDietHopper extends BlockHopper {
     private static final AxisAlignedBB top = makeAABB(0, 10, 0, 16, 16, 16);
     private static final AxisAlignedBB middle = makeAABB(4, 4, 4, 12, 10, 12);
-    
+
     private static final AxisAlignedBB[] bottom = new AxisAlignedBB[] {
-        makeAABB(6, 0, 6, 10, 4, 10),
-        makeAABB(0, 0, 0, 0, 0, 0),
-        makeAABB(6, 4, 0, 10, 8, 4),
-        makeAABB(6, 4, 12, 10, 8, 16),
-        makeAABB(0, 4, 6, 4, 8, 10),
-        makeAABB(12, 4, 6, 16, 8, 10)
+        makeAABB(6, 0, 6, 10, 4, 10), // DOWN
+        makeAABB(0, 0, 0, 0, 0, 0), // UP
+        makeAABB(6, 4, 0, 10, 8, 4), // NORTH
+        makeAABB(6, 4, 12, 10, 8, 16), // SOUTH
+        makeAABB(0, 4, 6, 4, 8, 10), // WEST
+        makeAABB(12, 4, 6, 16, 8, 10) // EAST
     };
 
     private static AxisAlignedBB makeAABB(int fromX, int fromY, int fromZ, int toX, int toY, int toZ) {
-        return AxisAlignedBB.getBoundingBox(-toX / 16F, -toY / 16F, -toZ / 16F, -fromX / 16F, -fromY / 16F, -fromZ / 16F);
+        return AxisAlignedBB.getBoundingBox(fromX / 16F, fromY / 16F, fromZ / 16F, toX / 16F, toY / 16F, toZ / 16F);
     }
 
     @Override
     public MovingObjectPosition collisionRayTrace(World world, int x, int y, int z, Vec3 start, Vec3 end) {
         int metadata = world.getBlockMetadata(x, y, z);
         int facing = getDirectionFromMetadata(metadata);
+
+        if (facing < 0 || facing >= bottom.length)
+            return null;
+
+        Vec3 pos = Vec3.createVectorHelper(x, y, z);
         
-        if (facing >= 0 && facing < bottom.length) {
-            Vec3 pos = Vec3.createVectorHelper(x, y, z);
-            Vec3 startRel = start.subtract(pos);
-            Vec3 endRel = end.subtract(pos);
-            
-            if (top.calculateIntercept(startRel, endRel) != null ||
-                middle.calculateIntercept(startRel, endRel) != null ||
-                bottom[facing].calculateIntercept(startRel, endRel) != null)
-            {
-                return super.collisionRayTrace(world, x, y, z, start, end);
-            }
+        // Vec3.subtract works backwards...
+        Vec3 startRel = pos.subtract(start);
+        Vec3 endRel = pos.subtract(end);
+
+        MovingObjectPosition relIntercept = collisionRel(facing, startRel, endRel);
+        if (relIntercept != null) {
+            return new MovingObjectPosition(x, y, z, relIntercept.sideHit, relIntercept.hitVec.addVector(x, y, z));   
         }
+
+        return null;
+    }
+
+    private static MovingObjectPosition collisionRel(int facing, Vec3 startRel, Vec3 endRel) {
+        MovingObjectPosition topIntercept = top.calculateIntercept(startRel, endRel);
+        if (topIntercept != null)
+            return topIntercept;
+
+        MovingObjectPosition middleIntercept = middle.calculateIntercept(startRel, endRel);
+        if (middleIntercept != null)
+            return middleIntercept;
+
+        MovingObjectPosition bottomIntercept = bottom[facing].calculateIntercept(startRel, endRel);
+        if (bottomIntercept != null)
+            return bottomIntercept;
 
         return null;
     }
